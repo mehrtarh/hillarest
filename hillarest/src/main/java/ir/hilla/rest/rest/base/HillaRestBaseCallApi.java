625716203f6adc6f8ce90ef275ec15d525d5349a
mehrtarh <@16399mehran16399@>
2019-11-05 09:35:31 +0330

    fixed bug to params

--- hillarest/src/main/java/ir/hilla/rest/rest/base/HillaRestBaseCallApi.java
+++ hillarest/src/main/java/ir/hilla/rest/rest/base/HillaRestBaseCallApi.java
@@ -16,12 +16,31 @@ public abstract class HillaRestBaseCallApi {
     }
 
     public final <Result> void get(String url, Class<Result> resultClass, List<HillaRestParamModel> params, HillaRestCallback<Result> callback) {
+        hillaHttpRestConnection.openConnection(url, resultClass, params, "GET", callback);
+    }
+
+    public final <Result> void get(String url, Class<Result> resultClass, HillaRestCallback<Result> callback) {
         hillaHttpRestConnection.openConnection(url, resultClass, "GET", callback);
     }
 
     public final <T, Result> void post(String url, Class<Result> resultClass, T bodyModel, List<HillaRestParamModel> params,
                                        HillaRestCallback<Result> callback) {
         hillaHttpRestConnection.openConnection(url, resultClass, bodyModel, params, "POST", callback);
+    }
+
+    public final <T, Result> void post(String url, Class<Result> resultClass, T bodyModel,
+                                       HillaRestCallback<Result> callback) {
+        hillaHttpRestConnection.openConnection(url, resultClass, bodyModel, "POST", callback);
+    }
+
+    public final <Result> void post(String url, Class<Result> resultClass,
+                                    HillaRestCallback<Result> callback) {
+        hillaHttpRestConnection.openConnection(url, resultClass, "POST", callback);
+    }
+
+    public final <Result> void post(String url, Class<Result> resultClass, List<HillaRestParamModel> params,
+                                    HillaRestCallback<Result> callback) {
+        hillaHttpRestConnection.openConnection(url, resultClass, params, "POST", callback);
     }
 
 
